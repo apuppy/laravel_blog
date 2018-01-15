17d870792de1070e73f2e71ad24cc9ef9c4dd2d3
Li Hongde <chinalihongde@163.com>
2018-01-15 23:56:51 +0800

    different route in Laravel

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/user', 'UserController@index');
+// basic route,treate with a Closure
+Route::get('simple',function(){
+    return 'Helllo World';
+});
+
+//specify a action to resolve the request
+Route::get('/user', 'UserController@index');
+
+//handel different request method
 Route::resource('/user', 'UserController');
+
+//match
+Route::match(['get', 'post'], '/match_request', function () {
+    return 'match request methods listed';
+});
+
+//any
+Route::any('any_request', function () {
+    return 'any request';
+});
+
+//redirect
+Route::redirect('/here', '/there', 301);
+
+//view route with variables
+Route::view('/hello','hello',['name' => 'Taylor']);
+
+//required parameters
+Route::get('/notice/{id}',function($id){
+    return 'User '.$id;
+});
+
+//optional parameters
+Route::get('/customer/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+// regular expression
+Route::get('/book/{name}', function ($name) {
+    return strtoupper($name);
+})->where('name', '[A-Za-z]+');
+Route::get('/phone/{id}', function ($id) {
+    return $id;
+})->where('id', '[0-9]+');
+Route::get('/company/{id}/{name}', function ($id, $name) {
+    return $id.' '.$name;
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
